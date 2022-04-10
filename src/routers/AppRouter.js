12696d862b265eebda1643806a88d84045b54283
alawa20 <huder13168@gmail.com>
2022-04-09 19:48:00 -0500

    router

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AuthScreen } from '../components/auth/AuthScreen'
+import { CalendarScreen } from '../components/calendar/CalendarScreen'
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <Route exact path="/auth" component={ AuthScreen } />
+      <Route exact path="/" component={ CalendarScreen } />
+    </Router>
+  )
+}
